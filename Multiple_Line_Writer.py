dc7f7d23bd8e6a08a59a4dee18e9431194563765
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-27 17:31:55 +0800

    header

---
+++ Multiple_Line_Writer.py
@@ -0,0 +1,12 @@
+# Paulean Marguerette F. Parrish
+# Bscpe 1-4
+# Problem no. 3
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+# Header design
+import pyfiglet
+
+from termcolor import colored
+from pyfiglet import Figlet
+f = Figlet(font = "digital")
+print(colored(f.renderText('Oh, Hello there!'), 'yellow'))
