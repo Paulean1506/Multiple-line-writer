bcd03b983c00ee0077f804e3ed1ff5859c6ffa84
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-27 18:34:58 +0800

    fixed error regarding indentation and string

--- Multiple_Line_Writer.py
+++ Multiple_Line_Writer.py
@@ -21,24 +21,24 @@ def write_to_file():
     # Open mylife.txt in write mode
     with open("mylife.txt", "w") as file:
 
-    # Ask user for input
-    line = input(str("\033[95mEnter line: "))
+        # Ask user for input
+        line = input(str("\033[95mEnter line: "))
 
-    # Writes the input into the file
-    file.write(line + "\n")
+        # Writes the input into the file
+        file.write(line + "\n")
 
-    # Asks user if they want to input more lines
-    add_lines = input(str("Are there more lines", user_name, "?"))
+        # Asks user if they want to input more lines
+        add_lines = input("Are there more lines y/n? ")
 
-    # Prompts user to only enter 'y' and 'n'
-    while not add_lines or add_lines[0].lower() not in ['y', 'n']:
-            add_lines=input(str("Invalid input, please only enter 'y', or 'n'"))
+        # Prompts user to only enter 'y' and 'n'
+        while not add_lines or add_lines[0].lower() not in ['y', 'n']:
+            add_lines=input("Invalid input, please only enter 'y', or 'n'")
 
-    # Allows user to input another line if 'y' is entered
+        # Allows user to input another line if 'y' is entered
         while add_lines.lower() == 'y':
-            line = input(str("Enter line: "))
+            line = input("Enter line: ")
             file.write(line + "\n")
-            add_lines = input(str("Are there more lines", user_name, "?"))
+            add_lines = input("Are there more lines y/n? ")
 
     # Ends the program if 'n' is entered
             if add_lines.lower() == 'n':
