e47109696b724790702f17028e3e4088d7ee44fe
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-04-27 17:56:07 +0800

    ask user if they want to input more lines

--- Multiple_Line_Writer.py
+++ Multiple_Line_Writer.py
@@ -24,5 +24,8 @@ with open("mylife.txt", "w") as file:
     # Ask user for input
     line = input(str("\033[95mEnter line: "))
 
-    #Writes the input into the file
+    # Writes the input into the file
     file.write(line + "\n")
+
+    # Asks user if they want to input more lines
+    more_lines = input(str("Are there more lines", user_name, "?"))
